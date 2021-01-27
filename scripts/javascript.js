aaa49496939c474e0d6075af8937960922ba3ed4
FIRST_NAME LAST_NAME <kjellberg.veronica@gmail.com>
2021-01-27 16:42:41 +0100

    first commit

---
+++ scripts/javascript.js
@@ -0,0 +1,37 @@
+var button = document.getElementById("button");
+var input = document.getElementById("input");
+var answer = document.getElementById("answer");
+var eight = document.getElementById("eight");
+var options = [
+    "As I see it, yes.",
+    "Ask again later.",
+    "Better not tell you now.",
+    "Cannot predict now.",
+    "Concentrate and ask again.",
+    "Don’t count on it.",
+    "It is certain.",
+    "It is decidedly so.",
+    "Most likely.",
+    "My reply is no.",
+    "My sources say no.",
+    "Outlook not so good.",
+    "Outlook good.",
+    "Reply hazy, try again.",
+    "Signs point to yes.",
+    "Very doubtful.",
+    "Without a doubt.",
+    "Yes.",
+    "Yes – definitely.",
+    "You may rely on it.",
+];
+
+button.addEventListener("click", function() {
+    if (input.value.length < 1) {
+        alert("Please enter a question!");
+    } else {
+        eight.innerText = "";
+        var num = input.value.length % options.length;
+        answer.innerText = options[num];
+    }
+
+});
